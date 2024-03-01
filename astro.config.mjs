6a4b8b74a9ac83c807bc46c10346bf525f5979c8
Manuel Schröder <manuel.schroeder@storyblok.com>
2024-03-01 17:03:53 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from 'astro/config'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
+import tailwind from '@astrojs/tailwind'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+const env = loadEnv('', process.cwd(), 'STORYBLOK')
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    storyblok({
+      //accessToken: env.STORYBLOK_TOKEN,
+      accessToken: 'Q01LlpdFFoXSgZ7kCrQoqAtt',
+      apiOptions: {
+        region: '',
+      },
+      bridge: {
+        customParent: 'https://app.storyblok.com',
+      },
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      },
+    }),
+    tailwind(),
+  ],
+  output: 'server',
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+})
